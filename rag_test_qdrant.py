350c01262b8d92e79241534d05498658c2c29350
Steffen Roecker <sroecker@redhat.com>
2024-05-14 17:20:39 +0200

    Add RAG test script

---
+++ rag_test_qdrant.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import logging
+
+from llama_index.core import Settings, VectorStoreIndex
+
+from qdrant_client import QdrantClient
+from llama_index.vector_stores.qdrant import QdrantVectorStore
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+#logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+
+from qdrant_client import QdrantClient
+# Qdrant local
+client = QdrantClient(
+    url="http://localhost:6334",
+    prefer_grpc=True,
+)
+
+# embeddings
+from llama_index.embeddings.ollama import OllamaEmbedding
+
+ollama_embedding = OllamaEmbedding(
+    model_name="mxbai-embed-large",
+    base_url="http://localhost:11434",
+    ollama_additional_kwargs={"mirostat": 0},
+)
+
+from llama_index.llms.ollama import Ollama
+llm = Ollama(model="llama3", request_timeout=120.0)
+
+
+Settings.llm = llm
+Settings.embed_model = ollama_embedding
+
+vector_store = QdrantVectorStore(client=client, collection_name="llamacpp", prefer_grpc=True)
+index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
+
+STREAMING = True
+query_engine = index.as_query_engine(
+    streaming=STREAMING,
+    verbose=True,
+)
+
+queries = [
+    "How to fix the Llama3 tokenizer?"
+]
+
+for query_text in queries:
+    response = query_engine.query(query_text)
+    print(response)
