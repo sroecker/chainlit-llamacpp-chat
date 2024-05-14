c7136fdff87df9bda123e8a02bc4cba6b6e83c38
Steffen Roecker <sroecker@redhat.com>
2024-05-14 17:32:32 +0200

    Change the RAG test script to a retriever

--- rag_test_qdrant.py
+++ rag_test_qdrant.py
@@ -3,6 +3,7 @@ import sys
 import logging
 
 from llama_index.core import Settings, VectorStoreIndex
+from llama_index.core.retrievers import VectorIndexRetriever
 
 from qdrant_client import QdrantClient
 from llama_index.vector_stores.qdrant import QdrantVectorStore
@@ -38,16 +39,34 @@ Settings.embed_model = ollama_embedding
 vector_store = QdrantVectorStore(client=client, collection_name="llamacpp", prefer_grpc=True)
 index = VectorStoreIndex.from_vector_store(vector_store=vector_store)
 
+queries = [
+    "How to add a new model arch?",
+    "Why is the first input token repeated?",
+    "How to fix the Llama3 tokenizer?"
+]
+
+#retriever = index.as_retriever()
+retriever = VectorIndexRetriever(
+    index=index,
+    similarity_topk=5,
+)
+
+response = retriever.retrieve(queries[0])
+for node in response:
+    print("node", node.score)
+    print("node", node.text)
+    print("node", node.metadata)
+    print("#####\n\n")
+
+"""
 STREAMING = True
 query_engine = index.as_query_engine(
     streaming=STREAMING,
     verbose=True,
 )
 
-queries = [
-    "How to fix the Llama3 tokenizer?"
-]
 
 for query_text in queries:
     response = query_engine.query(query_text)
     print(response)
+"""
